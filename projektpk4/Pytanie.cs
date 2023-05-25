f5ab958ee236806a5a25c14d4b28fc86cd84bcfc
Mily <92268823+ElMilos@users.noreply.github.com>
2023-05-25 12:10:05 +0200

    Add files via upload

--- projektpk4/Pytanie.cs
+++ projektpk4/Pytanie.cs
@@ -19,25 +19,53 @@ namespace projektpk4
         string OdpPrawidlowa;
 
 
-
-
-        static public void przypisanie()
-        {
-         string file = "E:\\Proejkt\\projektpk4\\projektpk4\\Quiz.txt";
-         string filetext=Czytnik.Czytaj(file);
-         int n = 0;
-         Regex r= new Regex("(@)");
-
-
-            string[] lines = filetext.Split('\n');
-
-            foreach (string line in lines)
+        public Pytanie(string pytaniee,string Odpa, string Odpb, string Odpc, string Odpd, string Odpprawidlowa)
             {
-                MessageBox.Show(line);
-                
+             pytanie =pytaniee;
+             OdpA = Odpa;
+             OdpB = Odpb;
+             OdpC = Odpc;
+             OdpD = Odpd;
+             OdpPrawidlowa = Odpprawidlowa;
+        }
+
+        static public void Quiz_List()
+        {
+         string A, B, C, D, Quest,Answer="";
+         string file = "C:\\Users\\Miłosz\\Desktop\\PK4\\projekt\\projektpk4\\Quiz.txt";
+         string filetext=Czytnik.Czytaj(file);
+
+         string pattern = "#";
+         Regex regex = new Regex(pattern);
+         
+
+            string[] lines = filetext.Split('\n'); // pętla przypisująca pytania i odp
+
+            for (int n =0;n<lines.Length;n++)
+            {
+          
+                 if(regex.IsMatch(lines[n]))
+                {
+                   Answer=regex.Replace(lines[n], "");
+                    lines[n] = Answer;
+
+                }
+
+                if (string.IsNullOrWhiteSpace(lines[n]))
+                {
+
+
+                    Quest = lines[n - 5];
+                    A=lines[n-4];
+                    B=lines[n-3];
+                    C=lines[n-2];
+                    D=lines[n-1];
+
+                    MessageBox.Show(Quest+A+B+C+D+Answer);
+                }
+
             }
 
-           // MessageBox.Show(filetext+1);
 
         }
 
